c9323fdfb7eb217c45aa6ace05db53363e55430b
letianw <tmac4ever0824@gmail.com>
2016-06-01 00:08:36 +1000

    closing #5

--- controllers.js
+++ controllers.js
@@ -1,3 +1,9 @@
 toDoApp.controller("ToDoController", function() {
   this.todos = [{task: "ToDo1", completed: true}, {task: "ToDo2", completed:false}];
+  this.addToDo = function (todoText){
+    this.todos.push({task: todoText, completed:false})
+  }
+  this.deleteToDo = function(item) {
+    this.todos.splice(this.todos.indexOf(item),1);
+  }
 });
